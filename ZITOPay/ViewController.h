33a498dc68b6d9c5e1ad599289a2fb1ed4836b61
lidongdong <964991296@qq.com>
2016-09-21 15:58:39 +0800

    Initial Commit

---
+++ ZITOPay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZITOPay
+//
+//  Created by 李冬冬 on 16/9/21.
+//  Copyright © 2016年 ldd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZITOPay/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ZITOPay
+//
+//  Created by 李冬冬 on 16/9/21.
+//  Copyright © 2016年 ldd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
