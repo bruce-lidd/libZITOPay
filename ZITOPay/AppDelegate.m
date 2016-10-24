925fda83a5df7617e882a3af3bcdd8f9a86c8f5f
李冬冬 <lidongdong@lidongdongdeiMac.local>
2016-10-24 10:47:17 +0800

    新建项目到github

--- ZITOPay/AppDelegate.m
+++ ZITOPay/AppDelegate.m
@@ -7,7 +7,8 @@
 //
 
 #import "AppDelegate.h"
-
+#import "ZITOPay.h"
+#import "ViewController.h"
 @interface AppDelegate ()
 
 @end
@@ -17,35 +18,34 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [ZITOPay initWithZitoID:ZITOID appId:APPID andAppSecret:APPKEY sandbox:NO];
+    
+    //weichat
+    [ZITOPay initWeChatPay:@""];
+    
+    ViewController *vc = [[ViewController alloc]init];
+    UINavigationController * nav = [[UINavigationController alloc]initWithRootViewController:vc];
+    
+    [self.window setRootViewController:nav];
+    [self.window makeKeyAndVisible];
+    
+    return YES;
+}
+//ios9之前使用这个方法
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    if (![ZITOPay handleOpenUrl:url]) {
+        //handle其他类型的url
+    }
     return YES;
 }
 
-
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+//iOS9之后官方推荐用此方法
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options {
+    NSLog(@"options %@", options);
+    if (![ZITOPay handleOpenUrl:url]) {
+        //handle其他类型的url
+    }
+    return YES;
 }
-
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-}
-
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
-
-
+                                                                                                       
 @end
--- ZITOPay/ViewController.m
+++ ZITOPay/ViewController.m
@@ -7,23 +7,135 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
-
+#import "ChannelTableViewCell.h"
+@interface ViewController ()<ZITOPayDelegate,UITableViewDelegate,UITableViewDataSource>
+@property (nonatomic,retain) NSMutableArray * channelDataArr;
 @end
 
 @implementation ViewController
-
+{
+    NSString * billTitle;//订单标题
+}
+- (void)viewWillAppear:(BOOL)animated
+{
+    #pragma mark - 设置delegate
+    [ZITOPay setZITOPayDelegate:self];
+}
+- (NSMutableArray *)channelDataArr
+{
+    if (!_channelDataArr) {
+        _channelDataArr = [[NSMutableArray alloc] init];
+    }
+    return _channelDataArr;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self setTitle:@"支付列表"];
     // Do any additional setup after loading the view, typically from a nib.
+    [self.view setBackgroundColor:[UIColor clearColor]];
+    
+    NSArray * channelArr = @[@{@"sub":@(PayChannelWxApp), @"img":@"wx", @"title":@"微信支付"},
+                             @{@"sub":@(PayChannelAli), @"img":@"ali", @"title":@"支付宝"},
+                             @{@"sub":@(PayChannelSumaQuick), @"img":@"suma", @"title":@"丰付快捷支付"},
+                             @{@"sub":@(PayChannelSumaUnion), @"img":@"suma", @"title":@"丰付银联支付"},
+                             @{@"sub":@(PayChannelChangQuick), @"img":@"chan", @"title":@"畅捷通快捷支付"},
+                                               ];
+    [self.channelDataArr addObjectsFromArray:channelArr];
+    
+    billTitle = [ZITOPay getCurrentMode] ? @"测试" : @"正式";
+    
+    UITableView * tableView = [[UITableView alloc]initWithFrame:self.view.frame style:UITableViewStylePlain];
+    [tableView setBackgroundColor:[UIColor whiteColor]];
+    [tableView setDelegate:self];
+    [tableView setDataSource:self];
+    [self.view addSubview:tableView];
+    [tableView reloadData];
+}
+#pragma mark - 微信、支付宝、丰付快捷、丰付银联、畅捷通
+
+- (void)doPay:(PayChannel)channel {
+    NSString *billno = [self genBillNo];
+    NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithObjectsAndKeys:@"value",@"key", nil];
+    ZITOPayReq *payReq = [[ZITOPayReq alloc] init];
+    payReq.channel = channel; //支付渠道
+    payReq.title = billTitle;//订单标题
+    payReq.currency = @"cny";//币种必填
+    payReq.totalFee = @"0.01";//订单价格;以分为单位的整数，即为0.1元
+    payReq.billNo = billno;//商户自定义订单号
+    payReq.scheme = @"paySchemes";//URL Scheme,在Info.plist中配置; 支付宝必有参数
+    payReq.billTimeOut = 300;//订单超时时间
+    payReq.viewController = self; //丰付快捷M＋、丰付银联必填
+    payReq.goodsname = @"西装";//商品名称，选填
+    payReq.goodsdetail = @"套装";//商品描述，选填
+    payReq.cardType = 0; //0 表示不区分卡类型；1 表示只支持借记卡；2 表示支持信用卡；默认为0
+    payReq.optional = dict;//商户业务扩展参数，会在webhook回调时返回
+   
+    [ZITOPay sendZITOReq:payReq];
+    
+}
+#pragma mark - ZITOPayDelegate
+- (void)onZITOPayResp:(ZITOBaseResp *)resp{
+     [self showAlertView:resp.resultMsg];
+}
+- (void)showAlertView:(NSString *)msg {
+    UIAlertView* alert = [[UIAlertView alloc]initWithTitle:@"提示" message:msg delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil, nil];
+    [alert show];
+}
+#pragma mark - 生成订单号
+- (NSString *)genBillNo {
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"yyyyMMddHHmmssSSS"];
+    return [formatter stringFromDate:[NSDate date]];
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+#pragma mark+++++++++++UITableViewDelegate+++++++++++++
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 120;
+}
+#pragma mark+++++++++++UITableViewDataSource+++++++++++++
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.channelDataArr count];
 }
 
+// Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+// Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
 
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString * cellIdentifier = @"cellIdentifier";
+    ChannelTableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    
+    if (!cell) {
+        cell = [[ChannelTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
+    NSDictionary * dic = [self.channelDataArr objectAtIndex:indexPath.row];
+    [cell setCellData:dic];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSDictionary * dic = [self.channelDataArr objectAtIndex:indexPath.row];
+    NSInteger channel = [[dic objectForKey:@"sub"] integerValue];
+    NSLog(@"%ld",channel);
+    switch (channel) {
+        case PayChannelWxApp:
+        case PayChannelAli:
+        case PayChannelAliApp:
+        case PayChannelSumaQuick:
+        case PayChannelSumaUnion:
+        case PayChannelChangQuick:
+        {
+            [self doPay:channel];
+        }
+            break;
+       
+        default:
+            break;
+    }
+}
 @end
